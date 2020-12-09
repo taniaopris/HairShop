abacfe2b81f6b45bc208327f2b84a3e91ba2ed20
taniaopris <48728948+taniaopris@users.noreply.github.com>
2020-12-09 17:44:01 +0200

    Update hairShop.h

--- src/hairShop.h
+++ src/hairShop.h
@@ -5,27 +5,34 @@
 #include<list>
 #include"product.h"
 
-class HairShop{
+class HairShop{ //Singleton class
 
 private:
     std::list<Product> products;
 
-    // Item 6: Explicitly disable the use of compiler generated functions you do not want
-    // In this case, copy ctor and copy assigment operator are declared private and without an implementation
+    //Item 14: Prohibit copying
+    //Copy ctor and copy assigment op are private because HairShop class is Singleton and there is no need for multiple instances of this class
     HairShop(const HairShop&);
     HairShop& operator = (const HairShop&);
 
-public:
+    static HairShop *instance;    
     //default ctor
     HairShop();
-
     //ctor with parameters
     HairShop(const std::list<Product>& prod);
 
+public:
+    
     //destructor
     ~HairShop();
     
     void listProducts(void);
+
+    //Item 14: Prohibit copying
+    static HairShop* getInstance(const std::list<Product>& prod);
+    //Item 14: Prohibit copying
+    static HairShop* getInstance();
+
 };
 
 #endif
